62ff4febff136eb2d8b3df4bf55839a4a9b600b0
Mariusz Ryndzionek <mryndzionek@gmail.com>
2017-08-02 18:30:26 +0200

    Moved applications to separate directories
    Adjusted the active signals detection mechanism

---
+++ apps/blink/include/signals.h
@@ -0,0 +1,14 @@
+#ifndef INCLUDE_SIGNALS_H_
+#define INCLUDE_SIGNALS_H_
+
+#define ESM_SIGNALS \
+		ESM_SIGNAL(tmout)
+
+#define ESM_IDS \
+      ESM_ID(blink)
+
+typedef struct
+{
+} esm_sig_params_t;
+
+#endif /* INCLUDE_SIGNALS_H_ */
