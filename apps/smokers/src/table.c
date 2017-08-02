60eeffcbab2e88097a1aa630675f8a95e95bfca1
Mariusz Ryndzionek <mryndzionek@gmail.com>
2017-08-02 18:32:34 +0200

    Added one more example application

---
+++ apps/smokers/src/table.c
@@ -0,0 +1,100 @@
+#include "esm/esm.h"
+#include "esm/esm_timer.h"
+
+#define N_SMOKERS		(3)
+
+ESM_THIS_FILE;
+
+typedef struct {
+} table_cfg_t;
+
+typedef struct {
+	esm_t esm;
+	esm_timer_t timer;
+    esm_t *philo[N_SMOKERS];
+    uint8_t fork[N_SMOKERS];
+    uint8_t is_hungry[N_SMOKERS];
+	table_cfg_t const *const cfg;
+} table_esm_t;
+
+#define ESM_INIT_SUB ( \
+		ESM_SIG_MASK(esm_sig_tmout) | \
+		ESM_SIG_MASK(esm_sig_done) \
+)
+
+ESM_DEFINE_STATE(idle);
+ESM_DEFINE_STATE(requesting);
+
+static void esm_idle_entry(esm_t *const esm)
+{
+	table_esm_t *self = ESM_CONTAINER_OF(esm, table_esm_t, esm);
+	esm_signal_t sig = {
+			.type = esm_sig_tmout,
+			.sender = esm,
+			.receiver = esm
+	};
+	esm_timer_add(&self->timer,
+			1000UL + ESM_RANDOM(5000UL), &sig);
+}
+
+static void esm_idle_exit(esm_t *const esm)
+{
+
+}
+
+static void esm_idle_handle(esm_t *const esm, esm_signal_t *sig)
+{
+	table_esm_t *self = ESM_CONTAINER_OF(esm, table_esm_t, esm);
+
+	switch(sig->type)
+	{
+	case esm_sig_tmout:
+		ESM_TRANSITION(&esm_requesting_state);
+		break;
+	default:
+		ESM_TRANSITION(&esm_unhandled_sig);
+		break;
+	}
+}
+
+static void esm_requesting_entry(esm_t *const esm)
+{
+	table_esm_t *self = ESM_CONTAINER_OF(esm, table_esm_t, esm);
+	smoker_resource_e resource = ESM_RANDOM(N_SMOKERS);
+
+	esm_signal_t sig = {
+			.type = esm_sig_request,
+			.sender = esm,
+			.receiver = (void *)0,
+			.params.resource = resource
+	};
+	esm_send_signal(&sig);
+
+	sig.params.resource = (resource + ESM_RANDOM(2) + 1) % N_SMOKERS;
+	esm_send_signal(&sig);
+}
+
+static void esm_requesting_exit(esm_t *const esm)
+{
+
+}
+
+static void esm_requesting_handle(esm_t *const esm, esm_signal_t *sig)
+{
+	table_esm_t *self = ESM_CONTAINER_OF(esm, table_esm_t, esm);
+
+	switch(sig->type)
+	{
+	case esm_sig_done:
+		ESM_TRANSITION(&esm_idle_state);
+		break;
+	default:
+		ESM_TRANSITION(&esm_unhandled_sig);
+		break;
+	}
+}
+
+static const table_cfg_t table_cfg = {
+};
+
+ESM_REGISTER(table, table, idle, N_SMOKERS);
