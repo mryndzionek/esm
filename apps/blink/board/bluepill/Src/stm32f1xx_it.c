676d0302297928aff6be38ca32551de00af0e735
Mariusz Ryndzionek <mryndzionek@gmail.com>
2017-09-01 18:38:35 +0200

    Added separate board configurations for every app

--- apps/blink/board/bluepill/Src/stm32f1xx_it.c
+++ apps/blink/board/bluepill/Src/stm32f1xx_it.c
@@ -36,7 +36,7 @@
 #include "stm32f1xx_it.h"
 
 /* USER CODE BEGIN 0 */
-#include "esm/esm.h"
+
 /* USER CODE END 0 */
 
 /* External variables --------------------------------------------------------*/
