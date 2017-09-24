f6f3236a1fea82b3f2cd65cc8a6cd060aef05241
Mariusz Ryndzionek <mryndzionek@gmail.com>
2017-09-24 10:18:15 +0200

    Updated module name to something that is more appropriate

--- modules/include/bus.h
+++ modules/include/bus.h
@@ -1,5 +1,5 @@
-#ifndef MODULES_INCLUDE_SYNCHRONIZER_H_
-#define MODULES_INCLUDE_SYNCHRONIZER_H_
+#ifndef MODULES_INCLUDE_BUS_H_
+#define MODULES_INCLUDE_BUS_H_
 
 #include "esm/esm.h"
 #include "esm/esm_queue.h"
@@ -12,14 +12,14 @@ typedef struct {
 	esm_queue_t *queue;
 	void (*request)(esm_signal_t *sig);
 	void (*respond)(esm_signal_t *sig);
-} synchronizer_cfg_t;
+} bus_cfg_t;
 
 typedef struct {
 	esm_t esm;
 	esm_timer_t timer;
-	synchronizer_cfg_t const *const cfg;
-} synchronizer_esm_t;
+	bus_cfg_t const *const cfg;
+} bus_esm_t;
 
-void esm_synchronizer_init(esm_t *const esm);
+void esm_bus_init(esm_t *const esm);
 
-#endif /* MODULES_INCLUDE_SYNCHRONIZER_H_ */
+#endif /* MODULES_INCLUDE_BUS_H_ */
