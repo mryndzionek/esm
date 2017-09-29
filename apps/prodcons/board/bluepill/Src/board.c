199a6c927c51852a32841628d0a44695096d9f89
Mariusz Ryndzionek <mryndzionek@gmail.com>
2017-09-29 18:54:22 +0200

    Added basic bluepill board support

---
+++ apps/prodcons/board/bluepill/Src/board.c
@@ -0,0 +1,15 @@
+#include "board.h"
+
+#include "bus.h"
+#include "esm/esm.h"
+
+static esm_list_t bus_q;
+
+static const bus_cfg_t bus_cfg = {
+		.timeout = 6000UL,
+		.req = esm_sig_request,
+		.rsp = esm_sig_response,
+		.queue = &bus_q,
+};
+
+ESM_REGISTER(bus, bus, esm_gr_none, 2);
