61d984f69b7d490e5a4e8de8c3f2848b08fe4555
Mariusz Ryndzionek <mryndzionek@gmail.com>
2019-06-30 08:51:48 +0200

    Added simple plasma effect

--- apps/clock/src/clock.c
+++ apps/clock/src/clock.c
@@ -7,7 +7,7 @@
 #include "sk6812.h"
 #include "board.h"
 
-#define NUM_MAJOR_STATES (4)
+#define NUM_MAJOR_STATES (5)
 
 #define OFF (0x00000000)
 #define RED (0x00AA0000)
@@ -49,6 +49,7 @@ typedef struct
 ESM_COMPLEX_STATE(active, top, 1);
 ESM_LEAF_STATE(time, active, 2);
 ESM_LEAF_STATE(fire, active, 2);
+ESM_LEAF_STATE(plasma, active, 2);
 ESM_LEAF_STATE(off, active, 2);
 
 ESM_COMPLEX_STATE(sunrise, active, 2);
@@ -157,7 +158,7 @@ static void render_time(ds3231_time_t const *time)
 	}
 }
 
-static void fire_flicker(int rev_intensity)
+void fire_flicker(int rev_intensity)
 {
 	uint8_t r = (FIRE_COLOR >> 16) & 0xFF;
 	uint8_t g = (FIRE_COLOR >> 8) & 0xFF;
@@ -170,12 +171,164 @@ static void fire_flicker(int rev_intensity)
 	}
 }
 
+static uint8_t sin8(uint8_t theta)
+{
+	static const uint8_t b_m16_interleave[] = {0, 49, 49, 41, 90, 27, 117, 10};
+	uint8_t offset = theta;
+
+	if (theta & 0x40)
+	{
+		offset = (uint8_t)255 - offset;
+	}
+	offset &= 0x3F; // 0..63
+
+	uint8_t secoffset = offset & 0x0F; // 0..15
+	if (theta & 0x40)
+		secoffset++;
+
+	uint8_t section = offset >> 4; // 0..3
+	uint8_t s2 = section * 2;
+	const uint8_t *p = b_m16_interleave;
+	p += s2;
+	uint8_t b = *p;
+	p++;
+	uint8_t m16 = *p;
+
+	uint8_t mx = (m16 * secoffset) >> 4;
+
+	int8_t y = mx + b;
+	if (theta & 0x80)
+		y = -y;
+
+	y += 128;
+
+	return y;
+}
+
+static uint8_t cos8(uint8_t theta)
+{
+	return sin8(theta + 64);
+}
+
+static uint16_t beat88(uint16_t beats_per_minute_88, uint32_t timebase)
+{
+    return ((esm_global_time - timebase) * beats_per_minute_88 * 280) >> 16;
+}
+
+static uint16_t beat16(uint16_t beats_per_minute, uint32_t timebase)
+{
+    if( beats_per_minute < 256) beats_per_minute <<= 8;
+    return beat88(beats_per_minute, timebase);
+}
+
+static uint8_t beat8(uint16_t beats_per_minute, uint32_t timebase)
+{
+    return beat16( beats_per_minute, timebase) >> 8;
+}
+
+static inline __attribute__((always_inline)) uint8_t scale8(uint8_t i, uint8_t scale)
+{
+	return (((uint16_t)i) * (1 + (uint16_t)(scale))) >> 8;
+}
+
+static inline __attribute__((always_inline)) uint8_t scale8_LEAVING_R1_DIRTY(uint8_t i, uint8_t scale)
+{
+	return (((uint16_t)i) * ((uint16_t)(scale) + 1)) >> 8;
+}
+
+static uint8_t beatsin8(uint16_t beats_per_minute, uint8_t lowest, uint8_t highest)
+{
+	uint32_t timebase = 0;
+	uint8_t phase_offset = 0;
+	uint8_t beat = beat8(beats_per_minute, timebase);
+	uint8_t beatsin = sin8(beat + phase_offset);
+	uint8_t rangewidth = highest - lowest;
+	uint8_t scaledbeat = scale8(beatsin, rangewidth);
+	uint8_t result = lowest + scaledbeat;
+	return result;
+}
+
+static uint8_t ease8InOutCubic(uint8_t i)
+{
+    uint8_t ii  = scale8_LEAVING_R1_DIRTY(  i, i);
+    uint8_t iii = scale8_LEAVING_R1_DIRTY( ii, i);
+
+    uint16_t r1 = (3 * (uint16_t)(ii)) - ( 2 * (uint16_t)(iii));
+
+    /* the code generated for the above *'s automatically
+       cleans up R1, so there's no need to explicitily call
+       cleanup_R1(); */
+
+    uint8_t result = r1;
+
+    if( r1 & 0x100 ) {
+        result = 255;
+    }
+    return result;
+}
+
+static uint8_t triwave8(uint8_t in)
+{
+	if (in & 0x80)
+	{
+		in = 255 - in;
+	}
+	uint8_t out = in << 1;
+	return out;
+}
+
+static uint8_t cubicwave8(uint8_t in)
+{
+    return ease8InOutCubic(triwave8( in));
+}
+
+static uint32_t color_wheel(uint8_t pos)
+{
+	pos = 255 - pos;
+	if (pos < 85)
+	{
+		return ((uint32_t)(255 - pos * 3) << 16) | ((uint32_t)(0) << 8) | (pos * 3);
+	}
+	else if (pos < 170)
+	{
+		pos -= 85;
+		return ((uint32_t)(0) << 16) | ((uint32_t)(pos * 3) << 8) | (255 - pos * 3);
+	}
+	else
+	{
+		pos -= 170;
+		return ((uint32_t)(pos * 3) << 16) | ((uint32_t)(255 - pos * 3) << 8) | (0);
+	}
+}
+
+#define qsuba(x, b) ((x > b) ? x - b : 0)
+#define pproc(x, b) ((x > b) ? x - b : 2)
+static void plasma(void)
+{
+	int thisPhase = beatsin8(4, -64, 64);
+	int thatPhase = beatsin8(5, -64, 64);
+
+	for (int k = 0; k < SK6812_LEDS_NUM; k++)
+	{
+		int colorIndex = cubicwave8((k * 3) + thisPhase) / 2 + cos8((k * 4) + thatPhase) / 2;
+		int thisBright = qsuba(colorIndex, beatsin8(7, 0, 128));
+
+		uint32_t c = color_wheel(colorIndex % 256);
+		uint8_t r = (c >> 16) & 0xFF;
+		uint8_t g = (c >> 8) & 0xFF;
+		uint8_t b = c & 0xFF;
+
+		sk6812_set_rgb(k, r, pproc(g, thisBright), pproc(b, thisBright));
+	}
+}
+
 static void esm_active_init(esm_t *const esm)
 {
 	clock_esm_t *self = ESM_CONTAINER_OF(esm, clock_esm_t, esm);
 	self->freq_hz = 10UL;
 	self->i = 0;
-	ESM_TRANSITION(time);
+	//esm->next_state = self->cfg->states[self->i];
+	ESM_TRANSITION(plasma);
 }
 
 static void esm_active_entry(esm_t *const esm)
@@ -426,6 +579,50 @@ static void esm_fire_handle(esm_t *const esm, const esm_signal_t *const sig)
 	}
 }
 
+static void esm_plasma_entry(esm_t *const esm)
+{
+	clock_esm_t *self = ESM_CONTAINER_OF(esm, clock_esm_t, esm);
+
+	self->freq_hz = 30UL;
+
+	esm_signal_t sig = {
+		.type = esm_sig_tmout,
+		.sender = esm,
+		.receiver = esm};
+	esm_timer_add(&self->timer,
+				  1000UL / self->freq_hz, &sig);
+}
+
+static void esm_plasma_exit(esm_t *const esm)
+{
+	clock_esm_t *self = ESM_CONTAINER_OF(esm, clock_esm_t, esm);
+	esm_timer_rm(&self->timer);
+}
+
+static void esm_plasma_handle(esm_t *const esm, const esm_signal_t *const sig)
+{
+	(void)esm;
+
+	switch (sig->type)
+	{
+	case esm_sig_tmout:
+	{
+		plasma();
+		esm_signal_t s = {
+			.type = esm_sig_alarm,
+			.sender = NULL,
+			.receiver = strip1_esm};
+		esm_send_signal(&s);
+		ESM_TRANSITION(self);
+	}
+	break;
+
+	default:
+		ESM_TRANSITION(unhandled);
+		break;
+	}
+}
+
 static void esm_off_entry(esm_t *const esm)
 {
 	(void)esm;
@@ -467,6 +664,7 @@ static const clock_cfg_t clock1_cfg = {
 		(esm_state_t const *const) & esm_time_state,
 		(esm_state_t const *const) & esm_sunrise_state,
 		(esm_state_t const *const) & esm_fire_state,
+		(esm_state_t const *const) & esm_plasma_state,
 		(esm_state_t const *const) & esm_off_state,
 	}};
 
