2873b9447c9fb6d7ab65af79136c6b0d71a2cf71
Mariusz Ryndzionek <mryndzionek@gmail.com>
2020-01-03 19:35:30 +0100

    Removed unused declaration

--- lib/include/sk6812.h
+++ lib/include/sk6812.h
@@ -3,7 +3,6 @@
 
 #include <stdint.h>
 
-void sk6812_init(void);
 void sk6812_set_rgb(uint16_t n, uint8_t r, uint8_t g, uint8_t b);
 void sk6812_set_color(uint16_t n, uint32_t c);
 void sk6812_set_brightness(uint8_t b);
