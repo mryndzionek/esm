2586cceb7b7b662457af660fff1c2917bd6d723d
Mariusz Ryndzionek <mryndzionek@gmail.com>
2020-10-25 10:14:56 +0100

    Added composite HID configuration for a pointer device

--- Middlewares/ST/STM32_USB_Device_Library/Class/HID/Src/usbd_hid.c
+++ Middlewares/ST/STM32_USB_Device_Library/Class/HID/Src/usbd_hid.c
@@ -364,6 +364,33 @@ __ALIGN_BEGIN static const uint8_t HID_KEYBOARD_ReportDesc[HID_KEYBOARD_REPORT_D
         0x09, 0xEA, //   Usage (Volume Decrement)
         0x81, 0x02, //   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
         0xC0,       // End Collection
+
+        0x05, 0x01, // USAGE_PAGE (Generic Desktop)
+        0x09, 0x02, // USAGE (Mouse)
+        0xa1, 0x01, // COLLECTION (Application)
+        0x85, 0x03, //   REPORT_ID (Mouse)
+        0x09, 0x01, //   USAGE (Pointer)
+        0xa1, 0x00, //   COLLECTION (Physical)
+        0x05, 0x09, //     USAGE_PAGE (Button)
+        0x19, 0x01, //     USAGE_MINIMUM (Button 1)
+        0x29, 0x02, //     USAGE_MAXIMUM (Button 2)
+        0x15, 0x00, //     LOGICAL_MINIMUM (0)
+        0x25, 0x01, //     LOGICAL_MAXIMUM (1)
+        0x75, 0x01, //     REPORT_SIZE (1)
+        0x95, 0x02, //     REPORT_COUNT (2)
+        0x81, 0x02, //     INPUT (Data,Var,Abs)
+        0x95, 0x06, //     REPORT_COUNT (6)
+        0x81, 0x03, //     INPUT (Cnst,Var,Abs)
+        0x05, 0x01, //     USAGE_PAGE (Generic Desktop)
+        0x09, 0x30, //     USAGE (X)
+        0x09, 0x31, //     USAGE (Y)
+        0x15, 0x81, //     LOGICAL_MINIMUM (-127)
+        0x25, 0x7f, //     LOGICAL_MAXIMUM (127)
+        0x75, 0x08, //     REPORT_SIZE (8)
+        0x95, 0x02, //     REPORT_COUNT (2)
+        0x81, 0x06, //     INPUT (Data,Var,Rel)
+        0xc0,       //   END_COLLECTION
+        0xc0        // END_COLLECTION
 };
 
 /**
